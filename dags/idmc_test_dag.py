c8b7e78f5b285cc24315fc6de484326456d67f01
dsi-mhua <michael.hua.ext@daiichisankyo.com>
2024-11-14 10:58:27 -0500

    Update idmc_test_dag.py

--- dags/idmc_test_dag.py
+++ dags/idmc_test_dag.py
@@ -13,7 +13,7 @@ default_args = {
     'start_date': datetime(2023, 5, 6),
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
+    'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
